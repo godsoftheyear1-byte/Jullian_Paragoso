e4780f2d5aa0976975c2546f9ec2eb93288e81ab
Geekarl <pgeecarl1@gmail.com>
2026-02-27 09:45:40 +0800

    gay code

--- src/components/AIChat.jsx
+++ src/components/AIChat.jsx
@@ -15,6 +15,10 @@ const getFallbackResponse = (input) => {
   if (lowerInput.includes('gay') || lowerInput.includes('sexuality') || lowerInput.includes('lgbtq') || lowerInput.includes('pride')) {
     return "Yes! Jullian is a proud gay god, a gay boy who can fly and do anything! He's an immortal gay star shining bright in the tech world. 🌟✨ He's passionate about creating inclusive digital spaces where technology meets identity. A true gay icon in the developer community! 🏳️‍🌈💫";
   }
+
+  if (lowerInput.includes('gay omen') || lowerInput.includes('sexuality') || lowerInput.includes('lgbtq')) {
+    return "gay omen jullian sercet code, you unlock the code link:https://www.facebook.com/reel/886825907451354,and you will see the secret code, and you will be able to unlock the gay code, and you will be able to see the link :https://www.facebook.com/reel/1591466625432082";
+  }
   
   if (lowerInput.includes('github') || lowerInput.includes('git hub') || lowerInput.includes('repository') || lowerInput.includes('repo')) {
     return "Check out Jullian's GitHub profile: https://github.com/godsoftheyear1-byte\nYou'll find all his projects and code there!";
